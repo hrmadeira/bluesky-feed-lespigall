d0030baf22bd886cb2ec1ace41075ae92e17bcc1
Hugo Rodrigues Madeira <hugo@localhost.localdomain>
2025-05-21 12:10:04 +0200

    Definir publisherDid diretamente no código

--- src/methods/describe-generator.ts
+++ src/methods/describe-generator.ts
@@ -1,14 +1,14 @@
-import { AppContext } from '../config'
+export default (server: any) => {
+  const publisherDid = 'did:web:bluesky-feed-lespigall.onrender.com'
 
-export default (server: any, ctx: AppContext) => {
   server.xrpc.method(
     'app.bsky.feed.describeFeedGenerator',
     async () => {
       return {
-        did: ctx.cfg.publisherDid,
+        did: publisherDid,
         feeds: [
           {
-            uri: `at://${ctx.cfg.publisherDid}/app.bsky.feed.generator/habitatge-cooperatiu`,
+            uri: `at://${publisherDid}/app.bsky.feed.generator/habitatge-cooperatiu`,
             displayName: 'Habitatge Cooperatiu Catalunya',
             description: 'Feed sobre cooperatives d’habitatge i dret a viure bé',
           },
